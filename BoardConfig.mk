7a5ccef341f49d09ea0ee00ec7c1e6247b928486
Suren Baghdasaryan <surenb@google.com>
2018-01-09 15:47:40 -0800

    surya: Set lmkd configuration properties
    Bug: 63631020
    
    Change-Id: I16a183e8e4625e8b98cefdc9c8a7b650556a2b55
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,6 +174,7 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
