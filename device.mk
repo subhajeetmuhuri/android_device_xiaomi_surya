0f6eb9eb7427851d9ab022a6c2d25f17ae7096e0
LuK1337 <priv.luk@gmail.com>
2019-11-22 01:14:07 +0100

    surya: Copy fstab to ramdisk

--- device.mk
+++ device.mk
@@ -174,6 +174,7 @@ PRODUCT_PACKAGES += \
 
 # Fstab
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
 
 # GPS
