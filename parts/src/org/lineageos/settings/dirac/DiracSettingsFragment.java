702b584341d187673de2fc578bd3a4a56715a847
Bruno Martins <bgcngm@gmail.com>
2020-05-16 12:10:14 +0100

    surya: parts: Prevent NPE before DiracSound is initialized
    There can be an edge case where the BOOT_COMPLETED intent is not
    yet received and user tries to open Dirac sound settings. Avoid
    letting it crash by making sure init is executed before showing the
    preference screen.
    
    Change-Id: I77e6bdd0c61994338f0dbbce4bfa532d84b8afb7

--- parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
+++ parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The LineageOS Project
+ * Copyright (C) 2018,2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -46,6 +46,7 @@ public class DiracSettingsFragment extends PreferenceFragment implements
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.dirac_settings);
 
+        DiracUtils.initialize();
         boolean enhancerEnabled = DiracUtils.isDiracEnabled(getActivity());
 
         mSwitchBar = (MainSwitchPreference) findPreference(PREF_ENABLE);
