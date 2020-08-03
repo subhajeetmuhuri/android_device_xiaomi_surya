9c0bee901ccf991366707b8b1115cc390d9bf12b
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-08-03 21:13:28 +0530

    surya: Switch to LLVM tools for kernel build
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -112,6 +112,8 @@ KERNEL_LD := LD=ld.lld
 
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_ADDITIONAL_FLAGS += \
+    AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 
 TARGET_KERNEL_APPEND_DTB := false
 TARGET_KERNEL_CLANG_COMPILE := true
