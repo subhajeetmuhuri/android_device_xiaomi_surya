a02e4f1efacbab9ada3f7f4d622c25732504032d
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    surya: Migrate to Xiaomi power HAL AIDL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- device.mk
+++ device.mk
@@ -327,7 +327,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.xiaomi-libperfmgr
 
 # QTI
 PRODUCT_PACKAGES += \
@@ -368,6 +368,8 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/xiaomi
 
 # Telephony
