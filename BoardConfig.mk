ffc7bbb379fcaf80ee28ceecd780acc534fddbf9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-03 00:27:07 +0200

    surya: Drop power AIDL extension
    Change-Id: Ied144ee0f7840ab43499d43f3d0c64dfd414d99f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,9 +159,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Platform
 TARGET_BOARD_PLATFORM := sm6150
 
-# Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
-
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
