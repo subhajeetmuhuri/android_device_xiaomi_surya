8ac76f71f63f1f894fa6b2325c03e4e707a279f3
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-09-28 13:37:24 +0530

    surya: Nuke msm_irqbalance
    Change-Id: I67ca3b1fe9a1ffef196832cada39f99bc4a8c83e

--- device.mk
+++ device.mk
@@ -243,10 +243,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/security/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
-# IRQ balance
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
