0ceb37aa0d70c72cbf1d88c7e6e85b3e6ec5dd6f
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-10-16 21:58:49 +0530

    surya: lineage_surya → aosp_surya

--- aosp_surya.mk
+++ aosp_surya.mk
@@ -21,10 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := aosp_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
