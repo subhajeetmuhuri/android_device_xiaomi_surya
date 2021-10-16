daab320a96523702c88c8a9edc20c98f3f78c844
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-10-16 21:53:21 +0530

    surya: PRODUCT_MODEL: POCO X3 NFC -> M2007J20CG

--- lineage_surya.mk
+++ lineage_surya.mk
@@ -27,7 +27,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO X3 NFC
+PRODUCT_MODEL := M2007J20CG
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
